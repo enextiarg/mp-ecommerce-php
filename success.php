3f0c838f8522a7518365d852fe5a7a7768e53a50
mromero <manuel@romero.com.ar>
2020-06-11 11:24:33 -0300

    Ajustes

---
+++ success.php
@@ -0,0 +1,13 @@
+<?php 
+$collection_id = $_GET['collection_id'];
+$collection_status = $_GET['collection_status'];
+$external_ref = $_GET['external_ref'];
+$payment_type = $_GET['payment_type'];
+$preference_id = $_GET['preference_id'];
+$site_id = $_GET['site_id'];
+$processing_mode = $_GET['processing_mode'];
+$merchant_account_id = $_GET['merchant_account_id'];
+
+?>
+
+El pago ha sido procesado correctamente.
